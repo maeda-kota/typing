4066b900567ba1e713416e9f2dabae48d63d9b7b
maeda-kota <maeda.kota.25s@st.kyoto-u.ac.jp>
2025-07-11 19:58:08 +0900

    x

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
   console.log(`サーバーを http://localhost:${PORT} で起動しました`);
 });
